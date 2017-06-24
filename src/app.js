579b0026b0d6892437759af0a52c2749cc649c95
Asai <Asai_Code@Asais-MacBook-Pro.local>
2017-06-23 17:12:00 -0700

    adding second page, socket.io, logic to form

--- src/app.js
+++ src/app.js
@@ -10,19 +10,38 @@ var complete = document.querySelector("#complete");
 var formMessage = document.querySelector('#form-message');
 var a = document.querySelector('a');
 
-function formCheck () {
-  if (firstName === true && lastName && true && email && true && password === true && username === true) {
-    // then we'll want to redirect them to the user screen
-    // different UI
+var password = password.addEventListener('keypress', function (e) {
+    // make sure that the password is AT LEAST 8 characters long
+    if (!e.target.value || e.target.value.length <= 8) {
+      return true;
+    console.log(e.target.value);
+    } else {
+      return false;
+    }
+},true);
+
+  var email = email.addEventListener('keypress', function (e) {
+    e.stopPropagation();
+    if (e.target.value.includes('@') && e.target.value.includes('.')) {
+      var result = result(e.target.value);
+      return result;
+      console.log(e.target.value);
+    } else {
+      return false;
+    }
+  },true);
+
+  console.log(email);
+
+  if (password && email) {
     complete.addEventListener('click', function () {
       window.location.assign(a);
     });
   } else {
-    // make sure they fill out the form
-      complete.addEventListener('click', function () {
-        formMessage.innerHTML = "Oops, looks like you didn't fully complete the registration form.";
-      });
-  }
-}
+    complete.addEventListener('click', function () {
+    formMessage.innerHTML = "Oops, looks like you didn't fully complete the registration form.";
+  });
+};
 
-formCheck();
+// passwordCheck();
+// formCheck();
